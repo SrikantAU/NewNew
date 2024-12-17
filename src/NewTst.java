ec46f361d3c7e7931045b3044852fc2bf94495b6
NewNew <Asrikant1@gmail.com>
2024-12-17 20:10:26 +0530

    Hero message

---
+++ src/NewTst.java
@@ -0,0 +1,18 @@
+
+public class NewTst {
+
+	public static void main(String[] args) {
+		String name="Srikant";
+		
+		if(name=="Srikant")
+		{
+			System.out.println("He is Hero");
+		}
+		else
+			
+		{
+			System.out.println("He is Hero+");
+		}
+	}
+
+}
